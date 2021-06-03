1b4e9f35d3ff61ab84b845c37b0b97a97cb4858a
Eduardo Penedo <eduardorodriguespenedo@gmail.com>
2021-06-02 23:00:06 -0300

    Funções de realização e validação de jogada adicionadas;Comentários explicado o código

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,4 +1,4 @@
-# cria tabuleiro com "-" dentro
+# Cria tabuleiro com "-" dentro
 def novo_tabuleiro():
     tabuleiro = list()
 
@@ -11,18 +11,22 @@ def novo_tabuleiro():
             tabuleiro[i].append("-")
     return tabuleiro
 
+# Mostra o tabuleiro
 def mostra_tabuleiro(tabuleiro):
     for i in range(len(tabuleiro)):
         # Mostra a linha gerada
         print(" ".join(tabuleiro[i]))
 
+# Verifica se há uma situação de vitória, retornando True quando há e False quando não há
 def ganhou(tabuleiro):
-    resposta = ()
+    resposta = (False,"-")
 
     # vitória horizontal e vertical
     for i in range(3):
+        # horizontal
         if (tabuleiro[i][0] == tabuleiro[i][1] and tabuleiro[i][0] == tabuleiro[i][2]):
             resposta = (True, tabuleiro[i][0])
+        # vertical
         if (tabuleiro[0][i] == tabuleiro[1][i] and tabuleiro[0][i] == tabuleiro[2][i]):
             resposta = (True, tabuleiro[0][i])
 
@@ -36,42 +40,76 @@ def ganhou(tabuleiro):
     if resposta[1] != '-':
         return resposta
     else:
-        return (False,)
+        return (False,"-")
 
+# Se há espaços em branco ("-") na matriz retorna True, caso contrário, retorna False 
 def espaco_em_branco(tabuleiro):
     ha_espaco = False
     for i in range(3):
         if "-" in tabuleiro[i]:
             ha_espaco = True
+    return ha_espaco
+
+# verifica se critérios que a jogada deve possuir
+def jogada_valida(jogada,tabuleiro):
+    valida = False
+    len_colunas = len(tabuleiro)
+    len_linhas = len(tabuleiro[0])
+    if len(jogada)==5 and jogada[0] == "[" and jogada[2] == "," and jogada[4] == "]":
+        x = int(jogada[1]) - 1
+        y = int(jogada[3]) - 1
+        if x<=len_linhas and y<=len_colunas and tabuleiro[y][x] == "-":
+            valida = True
+    return valida
+
+# Realiza a jogada pretendida
+def realizar_jogada(numero_jogador, tabuleiro, marcador):
+    # Pede jogada a ser feita
+    jogada = input(str.format("\nJogador {} - Escolha uma posição [x,y]\n> ", numero_jogador))
+
+    # Enquanto a jogada não for validada, pede outra entrada
+    while jogada_valida(jogada,tabuleiro) == False:
+        print("\nJogada inválida. Tente novamente ... \n")
+        jogada = input(str.format("\nJogador {} - Escolha uma posição [x,y]\n> ", numero_jogador))
+
+    # executa a jogada
+    x = int(jogada[1]) - 1      # considera que usuário irá fornecer entradas entre 1-3 e não de 0-2
+    y = int(jogada[3]) - 1      # considera que usuário irá fornecer entradas entre 1-3 e não de 0-2
+    tabuleiro[y][x] = marcador
+
+# inica um novo jogo
 def jogar():
+    # Inicia novo tabuleiro
     tabuleiro = novo_tabuleiro()
 
+    # Mostra tabuleiro
     mostra_tabuleiro(tabuleiro)
 
     # Enquanto houver espaços em branco ("-") o loop irá ocorrer
     while (espaco_em_branco(tabuleiro)):
+
         # Jogada jogador 1
-        jogada1 = input("Jogador 1 - Escolha uma posição [x,y]\n> ")
+        realizar_jogada(1, tabuleiro, "X")
 
-        x = int(jogada1[3]) - 1
-        y = int(jogada1[1]) - 1
-
-        tabuleiro[x][y] = "X"
-
+        # Mostra o tabuleiro após a jogada
         mostra_tabuleiro(tabuleiro)
 
+        # Verifica se ganhou
         if (ganhou(tabuleiro)[0]):
-            print(str(ganhou(tabuleiro)[1]) + " ganhou")
+            print("\n" + str(ganhou(tabuleiro)[1]) + " ganhou")
             break
 
         # Jogada jogador 2
-        jogada2 = input("Jogador 2 - Escolha uma posição [x,y]\n> ")
-        x = int(jogada2[3]) - 1
-        y = int(jogada2[1]) - 1
-        tabuleiro[x][y] = "O"
+        realizar_jogada(2, tabuleiro, "O")
 
+        # Mostra o tabuleiro após a jogada
         mostra_tabuleiro(tabuleiro)
-    
+
+        # Verifica se ganhou
+        if (ganhou(tabuleiro)[0]):
+            print("\n" + str(ganhou(tabuleiro)[1]) + " ganhou")
+            break
+
     # Verifica se deu velha 
     if espaco_em_branco(tabuleiro)==False and ganhou()==False:
         print("Velha!")
@@ -79,8 +117,11 @@ def jogar():
 if __name__ == '__main__':
     jogando = True
     while jogando:
+        # incia um novo jogo
         jogar()
-        continuarjogando = input("Deseja Jogar Novamente? S/N")
+
+        # Enquanto a resposta ao diálogo for diferente de "n" e "N" continua iniciando um novo jogo
+        continuarjogando = input("Deseja Jogar Novamente? S/N\n> ")
         if continuarjogando in ["N","n"]:
             jogando = False
     else:
